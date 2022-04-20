7d9bd7221d9d31e1ce4ee0db9e8c7493d85be20c
Zachary Rice <zricer@protonmail.com>
2022-04-20 17:55:07 -0500

    fix EOL in secret suffix (#847)
    * fix EOL in secret suffix
    
    * allow quoted key value syntax

--- cmd/generate/config/rules/generic.go
+++ cmd/generate/config/rules/generic.go
@@ -37,6 +37,9 @@ func GenericCredential() *config.Rule {
 	tps := []string{
 		generateSampleSecret("generic", "***REMOVED***"),
 		generateSampleSecret("generic", "Zf3D0LXCM3EIMbgJpUNnkRtOfOueHznB"),
+		`"client_id" : "0afae57f3ccfd9d7f5767067bc48b30f719e271ba470488056e37ab35d4b6506"`,
+		`"client_secret" : "6da89121079f83b2eb6acccf8219ea982c3d79bccc3e9c6a85856480661f8fde",`,
+		// TODO add more
 	}
 	return validate(r, tps)
 }
